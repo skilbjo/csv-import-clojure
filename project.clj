8e666814c1c203a32f2bfbeacb2ed63889f40318
John Skilbeck <john.skilbeck@gmail.com>
2016-07-18 15:24:53 -0700

    yay works!

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/java.jdbc "0.4.2"]
                  [org.postgresql/postgresql "9.3-1103-jdbc41"]
+                 [clj-time "0.11.0"]
                  [clojure-csv/clojure-csv "2.0.1"]]
   :main ^:skip-aot csv-import.core
   :target-path "target/%s"
--- src/csv_import/core.clj
+++ src/csv_import/core.clj
@@ -1,7 +1,10 @@
 (ns csv-import.core
   (:require [clojure.java.io :as io]
             [clojure-csv.core :as csv]
-            [clojure.java.jdbc :as sql])
+            [clojure.java.jdbc :as sql]
+            [clj-time.core :as t]
+            [clj-time.coerce :as c]
+            [clj-time.format :as f])
   (:gen-class))
 
 (def filepath "./data/direct_mail.promocodes22.csv")
@@ -10,29 +13,29 @@
          :subname "//docker:5432/direct_mail"
          :user "john.skilbeck"})
 
-; make this a vector of maps
-(defn insert-db 
-  [record]
-  (println [record])
-  (sql/insert! db
-    :promocodes2 [record]))
-;  (let [row (into [] (map record))]
-;    (println row)
-;    (sql/insert! db
-;      :promocodes2 row)))
-    
+(defn parse-int [s]
+  (Integer. (re-find #"\d+" s)))
+
+(defn alter-map [m fnmap]
+  (reduce (fn [m [k f]]
+    (assoc m k (f (m k))))
+      m fnmap))
+
+(defn transform [record]
+  (alter-map record {(keyword "\"Mail Date\"") c/to-sql-date,
+                     (keyword "\"Mailed Quantity\"") parse-int,
+                     (keyword "\"Actual Mailed Quantity\"") parse-int}))
+
+(defn insert-db [record]
+  (sql/insert! db :promocodes2 (transform record)))
 
 (defn parse-row [row]
   (let [v (first(csv/parse-csv row))]
-    (zipmap [:model 
-            :datasource 
-            (keyword "\"Add Suppresions\"")
-            (keyword "\"Selection Strategy Notes\"") 
-            (keyword "\"New/Remail\"")
-            (keyword "\"Mail Date\"")
-            (keyword "\"Mailed Quantity\"")
-            (keyword "\"Actual Mailed Quantity\"")
-            ] v)))
+    (zipmap 
+      [:model :datasource 
+       (keyword "\"Add Suppresions\"") (keyword "\"Selection Strategy Notes\"") 
+       (keyword "\"New Remail\"") (keyword "\"Mail Date\"") (keyword "\"Mailed Quantity\"") (keyword "\"Actual Mailed Quantity\"")
+      ] v)))
 
 (defn read-file [fname]
   (with-open [rdr (io/reader fname)]
